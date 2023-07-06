cd7bfbd6a3ca3f6783596cd95f4f8a29dc8e38f2
Macolister <bispo.mac@gmail.com>
2023-07-06 15:52:09 -0400

    pain

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import asc, desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,98 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = bakery.to_dict()
+        bakeries.append(bakery_dict)
+        # bakery_dict = {
+        #     "name" : bakery.name,
+        #     "bakery_id" : bakery.id,
+        #     "created_at" : bakery.created_at,
+        #     "updated_at": bakery.updated_at,
+        # }
+        # bakeries.append(bakery_dict)
+
+    response = make_response(
+        bakeries,
+        200
+    )
+    # print(bakeries)
+
+    # bakeries = Bakery.query.all()
+    # all_bakeries = bakeries.to_dict()
+    # response = make_response(
+    #     all_bakeries,
+    #     200
+    # )
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    bakery_dict = bakery.to_dict()
+    response = make_response(
+        bakery_dict,
+        200
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods_descending = BakedGood.query.order_by(asc(BakedGood.price)).all()
+
+    #its a list can't to_dict rip
+    # baked_price_dict = baked_goods_descending.to_dict()
+    #so break it up dummy
+
+    baked_goods = []
+    for baked_good in baked_goods_descending:
+        baked_good_dict = baked_good.to_dict()
+        baked_goods.append(baked_good_dict)
+
+        # baked_good_dict = {
+        #     "created_at" : baked_good.created_at,
+        #     "baked_good_id" : baked_good.id,
+        #     "name" : baked_good.name,
+        #     "updated_at": baked_good.updated_at,
+        #     "bakery_id": baked_good.bakery_id,
+        #     "price" : baked_good.price
+        # }
+        # baked_goods.append(baked_good_dict)
+
+    response = make_response(
+        baked_goods,
+        200
+    )
+    #I tried to also just pass the baked_goods list 
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_goods_expensive = BakedGood.query.order_by(desc(BakedGood.price)).limit(1).first()
+    baked_good_dict = baked_goods_expensive.to_dict()
+    
+    # baked_goods = []
+    # for baked_good in baked_goods_expensive:
+        # baked_goods.append(baked_good_dict)
+
+        # baked_good_dict = {
+        #     "created_at" : baked_good.created_at,
+        #     "baked_good_id" : baked_good.id,
+        #     "name" : baked_good.name,
+        #     "updated_at": baked_good.updated_at,
+        #     "bakery_id": baked_good.bakery_id,
+        #     "price" : baked_good.price
+        # }
+        # baked_goods.append(baked_good_dict)
+
+    response = make_response(
+        baked_good_dict,
+        200
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -12,9 +12,29 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    baked_goods = db.relationship('BakedGood', backref='bakery')
+
+    serialize_rules = ('-baked_goods.bakery',)
+
+    def __repr__(self):
+        return f'<Bakery : {self.name}>'
+    
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    serialize_rules = ('-bakery.baked_goods',)
     
+    def __repr__(self):
+        return f'<BakedGood: {self.name}. Cost: {self.price}>'
